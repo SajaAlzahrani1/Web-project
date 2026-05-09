dd41dc3ee02c0f366c428c92042f82838ecd37d8
Hessa <156654094+HHD7@users.noreply.github.com>
2026-05-09 18:53:08 +0300

    infinity free database

--- datab.php
+++ datab.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "discover_saudi";
+$host = "sql102.infinityfree.com";
+$user = "if0_41021107";
+$pass = "IoBiWwRYmcrG";
+$dbname = "if0_41021107_discover_saudi";
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 
